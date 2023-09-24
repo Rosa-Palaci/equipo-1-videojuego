d38e7b27ce63a06a8ef9bd853ab881f8eda1c886
Omar <tic2omar17@gmail.com>
2023-09-23 23:56:04 -0600

    juego
    juego funcionamiento basico

---
+++ juego/Assets/scripts/textos.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class textos : MonoBehaviour
+{
+
+    public TMP_Text pregunta;
+
+    public TMP_Text op3;
+    public TMP_Text op1;
+    public TMP_Text op2;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        string nombreDeEscenaActual = SceneManager.GetActiveScene().name;
+        
+        if(nombreDeEscenaActual == "principal")
+        {
+            pregunta.text = "Cual es el planeta mas grande?"; // p1q
+            op1.text = "principal"; //corecta
+            op2.text = "principal";
+            op3.text = "principal"; 
+            
+        }
+        if (nombreDeEscenaActual == "uno")
+        {
+            pregunta.text = "Cual es el planeta mas grande?"; // p2q
+            op1.text = "uno"; 
+            op2.text = "uno"; //corecta
+            op3.text = "uno";
+        }
+        if (nombreDeEscenaActual == "dos")
+        {
+            pregunta.text = "Cual es el planeta mas grande?"; // p3q
+            op1.text = "dos"; 
+            op2.text = "dos";
+            op3.text = "dos";
+        }
+        if (nombreDeEscenaActual == "tres")
+        {
+            pregunta.text = "Cual es el planeta mas grande?"; // p4q
+            op1.text = "tres"; 
+            op2.text = "tres";
+            op3.text = "tres";
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+}
+}
